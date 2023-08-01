9ed5ba86368e3325885c7dce9a5bc50ef466e042
Rehaan1 <rehaanmazid@gmail.com>
2023-08-01 21:34:47 +0530

    feat: delete user

--- auth/api/routes/users.js
+++ auth/api/routes/users.js
@@ -133,4 +133,60 @@ router.patch('/update', tokenCheck, (req, res) => {
     })
 })
 
+router.delete('/remove', tokenCheck, (req, res) => {
+  const userId = req.userId
+
+  dbUserPool.connect()
+    .then(client => {
+      client.query("BEGIN")
+        .then(() => {
+          const deleteQuery = format(
+            "DELETE FROM users WHERE user_id = %L RETURNING *",
+            userId
+          )
+
+          client.query(deleteQuery)
+            .then(result => {
+              if (result.rows.length === 0) {
+                client.query("ROLLBACK")
+                client.release()
+                return res.status(404).json({
+                  message: "User not found"
+                })
+              }
+
+              client.query("COMMIT")
+              client.release()
+              return res.status(200).json({
+                message: "User removed successfully"
+              })
+            })
+            .catch(err => {
+              client.query("ROLLBACK")
+              client.release()
+              console.log("Error: ", err)
+              return res.status(500).json({
+                message: "Query error",
+                error: err
+              })
+            })
+        })
+        .catch(err => {
+          console.log("Error: ", err)
+          client.release()
+          return res.status(500).json({
+            message: "Database transaction error",
+            error: err
+          })
+        })
+    })
+    .catch(err => {
+      console.log(err)
+      return res.status(500).json({
+        message: "Database Connection Error",
+        error: err
+      })
+    })
+})
+
 module.exports = router
