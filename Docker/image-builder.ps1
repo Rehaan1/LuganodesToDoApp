7576a6b9eae10c75bbcfa0a9a962c65eedab05e2
Rehaan1 <rehaanmazid@gmail.com>
2023-08-01 14:40:40 +0530

    feat: dockerize todo app

---
+++ Docker/image-builder.ps1
@@ -0,0 +1,2 @@
+# Build Docker image for todo service
+docker build -t luganodesTodo/todo-service ../todo
---
+++ Docker/image-builder.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Build Docker image for todo service
+docker build -t luganodesTodo/todo-service ../todo
