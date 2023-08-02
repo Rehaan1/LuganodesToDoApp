1a1c52a70febdaba96de580e682df93c30f2f427
Rehaan1 <rehaanmazid@gmail.com>
2023-08-02 09:01:09 +0530

    feat: frontend basic todo app

--- Docker/image-builder.ps1
+++ Docker/image-builder.ps1
@@ -3,3 +3,6 @@ docker build -t luganodestodo/todo-service ../todo
 
 # Build Docker image for auth service
 docker build -t luganodestodo/auth-service ../auth
+
+# Build Docker image for frontend service
+docker build -t luganodestodo/frontend-service ../frontend
